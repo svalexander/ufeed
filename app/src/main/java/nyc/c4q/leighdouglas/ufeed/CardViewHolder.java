32503a62d8b21eab522642d518db836fa8d0618f
Leigh Douglas <leighcdouglas1@gmail.com>
2016-10-30 17:21:38 -0400

    added recycler view

---
+++ app/src/main/java/nyc/c4q/leighdouglas/ufeed/CardViewHolder.java
@@ -0,0 +1,26 @@
+package nyc.c4q.leighdouglas.ufeed;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by leighdouglas on 10/30/16.
+ */
+
+public class CardViewHolder extends RecyclerView.ViewHolder {
+
+
+    public CardViewHolder(ViewGroup parent){
+        super(inflateView(parent));
+    }
+
+    public static View inflateView(ViewGroup parent){
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        return inflater.inflate(R.layout.recycled_card, parent, false);
+    }
+
+    public void bind(Integer integer){
+
+    }
+}
--- app/src/main/java/nyc/c4q/leighdouglas/ufeed/MainActivity.java
+++ app/src/main/java/nyc/c4q/leighdouglas/ufeed/MainActivity.java
@@ -2,12 +2,19 @@ package nyc.c4q.leighdouglas.ufeed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Adapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.our_recycler_view);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this)); //determines how content shows up
+        recyclerView.setAdapter(adapter = new Adapter());
     }
 }
