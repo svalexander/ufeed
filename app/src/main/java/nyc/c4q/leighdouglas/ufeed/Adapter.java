39f91d3cf8d1b9eaf00a99d382a113a3cb6519be
Leigh Douglas <leighcdouglas1@gmail.com>
2016-11-18 18:20:23 -0500

    Merge branch 'master' of /Users/leighdouglas/Desktop/accesscode/UFeed with conflicts.

--- app/src/main/java/nyc/c4q/leighdouglas/ufeed/Adapter.java
+++ app/src/main/java/nyc/c4q/leighdouglas/ufeed/Adapter.java
@@ -1,8 +1,10 @@
 package nyc.c4q.leighdouglas.ufeed;
 
+import android.support.annotation.IntegerRes;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,13 +12,13 @@ import java.util.List;
  * Created by leighdouglas on 10/30/16.
  */
 
-public class Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class Adapter extends RecyclerView.Adapter {
     private List<Integer> mData = Arrays.asList(
-            1, 2, 3);
+            1, 2, 3, 4);
 
     @Override
     public int getItemViewType(int position) {
-        switch(position){
+        switch(mData.get(position)){
             case 0:
                 return 1;
             case 1:
