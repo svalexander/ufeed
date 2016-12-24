8084fb75a95d38e49d189f95ca5d28dcb8b47cbb
Shannon Alexander-Navarro <shannonalexander@hotmail.com>
2016-12-24 00:08:28 -0500

    started gratitude card

---
+++ app/src/main/java/nyc/c4q/leighdouglas/ufeed/grateful_card/GraditudeMainActivity.java
@@ -0,0 +1,13 @@
+package nyc.c4q.leighdouglas.ufeed.grateful_card;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GraditudeMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graditude_main);
+    }
+}
